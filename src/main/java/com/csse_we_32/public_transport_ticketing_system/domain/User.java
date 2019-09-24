2f531368013e928094ede4410fed0c4ab77f837f
sanjaya jayasinghe <sanjayajayasinghe54@gmail.com>
2019-09-24 22:35:38 +0530

    conflict

--- src/main/java/com/csse_we_32/public_transport_ticketing_system/domain/User.java
+++ src/main/java/com/csse_we_32/public_transport_ticketing_system/domain/User.java
@@ -3,11 +3,14 @@ package com.csse_we_32.public_transport_ticketing_system.domain;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.validation.constraints.Size;
+import java.util.Collection;
 
 @Document(collection = "users")
-public class User {
+public class User implements UserDetails {
 
 
     @Id
@@ -24,7 +27,7 @@ public class User {
 
     private String email;
 
-    private String creditCardNo;
+   // private String creditCardNo;
 
     @Size(max = 11)
     private String nic;
@@ -32,34 +35,33 @@ public class User {
     @Size(max = 11,min = 10)
     private String contactNo;
     
-    private UserType userType;
+   // private UserType userType;
+
+    private Collection<? extends GrantedAuthority> authorities;//type
 
     
     
     
-    public User(String username, String password, String email, String creditCardNo, @Size(max = 11) String nic,
-			@Size(max = 11, min = 10) String contactNo, UserType userType) {
+    public User(String username, String password, String email, @Size(max = 11) String nic,
+			@Size(max = 11, min = 10) String contactNo, Collection<? extends GrantedAuthority> authorities) {
 		super();
 		this.username = username;
 		this.password = password;
 		this.email = email;
-		this.creditCardNo = creditCardNo;
+		//this.creditCardNo = creditCardNo;
 		this.nic = nic;
 		this.contactNo = contactNo;
-		this.userType = userType;
+		//this.userType = userType;
+		this.authorities=authorities;
 	}
 
-	public String getUsername() {
-        return username;
-    }
+
 
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
-    }
+
 
     public void setPassword(String password) {
         this.password = password;
@@ -82,13 +84,13 @@ public class User {
     }
 
 
-    public String getCreditCardNo() {
-        return creditCardNo;
-    }
-
-    public void setCreditCardNo(String creditCardNo) {
-        this.creditCardNo = creditCardNo;
-    }
+//    public String getCreditCardNo() {
+//        return creditCardNo;
+//    }
+//
+//    public void setCreditCardNo(String creditCardNo) {
+//        this.creditCardNo = creditCardNo;
+//    }
 
     public String getNic() {
         return nic;
@@ -98,17 +100,67 @@ public class User {
         this.nic = nic;
     }
 
-	public UserType getUserType() {
-		return userType;
-	}
-
-	public void setUserType(UserType userType) {
-		this.userType = userType;
-	}
+//	public UserType getUserType() {
+//		return userType;
+//	}
+//
+//	public void setUserType(UserType userType) {
+//		this.userType = userType;
+//	}
 
 	public void setId(String id) {
 		this.id = id;
 	}
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+
+
+
     
     
 }
--- src/main/java/com/csse_we_32/public_transport_ticketing_system/security/JwtUserDetailsService.java
+++ src/main/java/com/csse_we_32/public_transport_ticketing_system/security/JwtUserDetailsService.java
@@ -4,8 +4,8 @@ package com.csse_we_32.public_transport_ticketing_system.security;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.csse_we_32.public_transport_ticketing_system.domain.User;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -22,7 +22,11 @@ public class JwtUserDetailsService implements UserDetailsService {
 			//return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
 			//		new ArrayList<>());
 			
-			 return new JwtUser("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", "admin",AuthorityUtils.createAuthorityList("ADMIN"));
+
+			// return new JwtUser("admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", "admin",AuthorityUtils.createAuthorityList("ADMIN"));
+		    return new User("admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", "nishi@gmail.com", "971711713v",
+                    "0789195560", AuthorityUtils.createAuthorityList("ADMIN"));
+
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
 		}
