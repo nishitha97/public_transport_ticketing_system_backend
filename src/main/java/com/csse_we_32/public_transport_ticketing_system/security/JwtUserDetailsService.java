94ced2911f13f02477a12749d892942460849bf1
sanjaya jayasinghe <sanjayajayasinghe54@gmail.com>
2019-09-25 00:38:12 +0530

    user security no working

--- src/main/java/com/csse_we_32/public_transport_ticketing_system/security/JwtUserDetailsService.java
+++ src/main/java/com/csse_we_32/public_transport_ticketing_system/security/JwtUserDetailsService.java
@@ -4,6 +4,8 @@ package com.csse_we_32.public_transport_ticketing_system.security;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.csse_we_32.public_transport_ticketing_system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,16 +15,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
+	@Autowired
+	UserService userService;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		if ("admin".equals(username)) {
+
+		if (userService.getUserByUserName(username)!=null) {
 			//List<String> list=new ArrayList<>();
 			//list.add("admin");
 			//return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
 			//		new ArrayList<>());
-			
-			 return new JwtUser("admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", "admin",AuthorityUtils.createAuthorityList("ADMIN"));
+			return userService.getJWtUser(username);
+			// return new JwtUser("admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", "admin",AuthorityUtils.createAuthorityList("ADMIN"));
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
 		}
--- src/main/java/com/csse_we_32/public_transport_ticketing_system/service/impl/UserServiceImpl.java
+++ src/main/java/com/csse_we_32/public_transport_ticketing_system/service/impl/UserServiceImpl.java
@@ -2,11 +2,16 @@ package com.csse_we_32.public_transport_ticketing_system.service.impl;
 
 import com.csse_we_32.public_transport_ticketing_system.domain.User;
 import com.csse_we_32.public_transport_ticketing_system.repository.UserRepository;
+import com.csse_we_32.public_transport_ticketing_system.security.JwtUser;
 import com.csse_we_32.public_transport_ticketing_system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,9 +21,12 @@ public class UserServiceImpl implements UserService {
     UserRepository userRepository;
 
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
+
+
     @Override
     public User createUser(@Valid User user) {
-
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
@@ -41,7 +49,17 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public User getUserByUserName(String username) {
+        return userRepository.findByUsername(username);
+    }
+    @Override
     public void deleteUser(String userId) {
         userRepository.deleteById(userId);
+    }
+
+    @Override
+    public JwtUser getJWtUser(String username) {
+        User user= userRepository.findByUsername(username);
+        return new JwtUser(user.getUsername(),user.getPassword(),user.getUserType().toString(),AuthorityUtils.createAuthorityList(user.getUserType().toString()));
     }
 }
