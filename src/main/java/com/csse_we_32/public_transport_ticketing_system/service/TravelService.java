dc959b44719943b06ce0ab3e641b1f8c5b866365
sanjaya jayasinghe <sanjayajayasinghe54@gmail.com>
2019-09-28 15:50:12 +0530

    ticket service add

--- src/main/java/com/csse_we_32/public_transport_ticketing_system/service/TravelService.java
+++ src/main/java/com/csse_we_32/public_transport_ticketing_system/service/TravelService.java
@@ -3,7 +3,9 @@ package com.csse_we_32.public_transport_ticketing_system.service;
 import com.csse_we_32.public_transport_ticketing_system.domain.Travel;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TravelService {
     List<Travel> getAllTravels();
+    Optional<Travel> getTravelById(String id);
 }
