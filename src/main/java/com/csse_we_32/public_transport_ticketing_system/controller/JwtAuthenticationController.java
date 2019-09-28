70bbf5525b39a6f41cba18f7c8a9a085db77a7ce
sanjaya jayasinghe <sanjayajayasinghe54@gmail.com>
2019-09-28 18:57:21 +0530

    add auth

--- src/main/java/com/csse_we_32/public_transport_ticketing_system/controller/JwtAuthenticationController.java
+++ src/main/java/com/csse_we_32/public_transport_ticketing_system/controller/JwtAuthenticationController.java
@@ -1,6 +1,8 @@
 package com.csse_we_32.public_transport_ticketing_system.controller;
 
 import com.csse_we_32.public_transport_ticketing_system.security.*;
+import com.csse_we_32.public_transport_ticketing_system.service.AuthService;
+import com.csse_we_32.public_transport_ticketing_system.service.impl.AuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,35 +16,14 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class JwtAuthenticationController {
     @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    private JwtUserDetailsService userDetailsService;
+    AuthServiceImpl authService;
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-
-        final JwtUser userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-
-        final String token = jwtTokenUtil.generateToken(userDetails);
-
-        return ResponseEntity.ok(new JwtResponse(token));
+        return authService.createAuthenticationToken(authenticationRequest);
     }
 
-    private void authenticate(String username, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
-    }
+
 
 }
